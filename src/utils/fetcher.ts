55a1e441fb6fe5264b219401397ac1bea3b0c35d
isaacmsl <isaacmarlondasilvalourenco@gmail.com>
2021-03-01 01:01:21 -0300

    VERCEL_URL no fetcher

--- src/utils/fetcher.ts
+++ src/utils/fetcher.ts
@@ -1,5 +1,5 @@
 const fetcher = async (resource: string) => {
-    const res = await fetch(`http://${process.env.NEXT_PUBLIC_VERCEL_URL}${resource}`)
+    const res = await fetch(`http://${process.env.VERCEL_URL}${resource}`)
     const data = await res.json()
     return data
 }
